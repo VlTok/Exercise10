6d1643d476d247bbb8d583991e80e118ef6ae19f
Vladimir <57016594+VlTok@users.noreply.github.com>
2019-12-10 14:25:17 +0300

    Create formScript.js

---
+++ formScript.js
@@ -0,0 +1,34 @@
+var opacity = 0;
+var intervalID = 0;
+
+function fadeOut() {
+	intervalID = setInterval(hide, 40);
+}
+
+function fadeIn() {
+	intervalID = setInterval(show, 40);
+}
+
+function show() {
+	var div1 = document.getElementById("feedbackFormModal");
+	opacity = Number(window.getComputedStyle(div1).getPropertyValue("opacity"));
+	if (opacity < 1) {
+		opacity = opacity + 0.2;
+		div1.style.opacity = opacity;
+		console.log(opacity);
+	} else {
+		clearInterval(intervalID);
+	}
+}
+
+function hide() {
+	var div2 = document.getElementById("feedbackFormModal");
+	opacity = Number(window.getComputedStyle(div2).getPropertyValue("opacity"));
+	if (opacity > 0) {
+		opacity = opacity - 0.2;
+		div2.style.opacity = opacity;
+		console.log(opacity);
+	} else {
+		clearInterval(intervalID);
+	}
+}
